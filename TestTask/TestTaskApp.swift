c4138718b1ebc80e51f37239b654e24582cc199b
effeto <effectdopler@gmail.com>
2023-04-22 12:14:22 +0300

    Initial Commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Демьян on 22.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
